f453233e2cf9855fa72be91d18155fd2fbfe72a1
Nicole Garcia <nicole@nicoleiliana.com>
2025-01-16 11:50:04 -0800

    Initial commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Nicole on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Moonshot/MoonshotTests/MoonshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotTests.swift
+//  MoonshotTests
+//
+//  Created by Nicole on 1/16/25.
+//
+
+import Testing
+@testable import Moonshot
+
+struct MoonshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
